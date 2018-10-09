5620b21f5c2bdf4bc94e21fbab4d413fb5069764
meya <yazganmelih@outlook.com>
2018-10-09 14:41:48 +0200

    First commit/Submitting

---
+++ src/tools.h
@@ -0,0 +1,32 @@
+#ifndef TOOLS_H_
+#define TOOLS_H_
+#include <vector>
+#include "Eigen/Dense"
+#include <iostream>
+#include <fstream>
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using namespace std;
+
+class Tools {
+public:
+  /**
+  * Constructor.
+  */
+  Tools();
+
+  /**
+  * Destructor.
+  */
+  virtual ~Tools();
+
+  /**
+  * A helper method to calculate RMSE.
+  */
+  VectorXd CalculateRMSE(const vector<VectorXd> &estimations, const vector<VectorXd> &ground_truth);
+  //void writelog(double Nisvalues, bool sensor, ofstream & outfile);
+
+};
+
+#endif /* TOOLS_H_ */
